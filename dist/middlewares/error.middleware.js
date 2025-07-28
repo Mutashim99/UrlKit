09625405d42da2d758420b754d049fe8858679fc
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-07-29 04:46:11 +0500

    remove dist folder from tracking

--- dist/middlewares/error.middleware.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    console.log("Error Handler:", err);
-    const { status, message } = err;
-    res.status(status || 500).json({
-        success: false,
-        message: message || "Something went wrong"
-    });
-};
