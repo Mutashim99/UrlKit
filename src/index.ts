f4f1be558da18cc4018a67dac18af92d0037799b
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-08-15 17:41:15 +0500

    fix: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,34 @@
-import express from 'express'
-import 'dotenv/config.js'
-import cookieParser from 'cookie-parser';
-import { authenticate, optionalAuth } from './middlewares/authenticate.middleware.js'
-import { errorHandler } from './middlewares/error.middleware.js'
-import { authRouter } from './routes/auth.route.js'
-import {urlRouter} from './routes/url.route.js'
-import userRouter from './routes/user.route.js';
+import express from "express";
+import "dotenv/config.js";
+import cookieParser from "cookie-parser";
+import {
+  authenticate,
+  optionalAuth,
+} from "./middlewares/authenticate.middleware.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+import { authRouter } from "./routes/auth.route.js";
+import { urlRouter } from "./routes/url.route.js";
+import userRouter from "./routes/user.route.js";
+import cors from "cors";
 
-import './workers/email.worker.js'; // for worker to run //commenting this out cause ran out of free limits on upstash for redis cloud :(
+import "./workers/email.worker.js"; // for worker to run //commenting this out cause ran out of free limits on upstash for redis cloud :(
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
-app.use("/api/auth",authRouter)
-app.use('/api/url',optionalAuth ,urlRouter)
-app.use('/api/user',authenticate, userRouter)
-app.use(errorHandler)
-app.listen(PORT,()=>{
-    console.log(`running on ${PORT} `);
-})
+app.use("/api/auth", authRouter);
+app.use("/api/url", optionalAuth, urlRouter);
+app.use("/api/user", authenticate, userRouter);
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`running on ${PORT} `);
+});
