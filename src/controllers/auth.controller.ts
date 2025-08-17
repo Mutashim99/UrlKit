031e1bd6b6c5287c18f4621c1c32b889c9e99c42
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-08-17 14:21:07 +0500

    fix: update SameSite attribute for cookies to 'none' for cross-site usage

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -121,7 +121,7 @@ export const login = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
     res.status(200).send({
